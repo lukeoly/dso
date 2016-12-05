ac5f14289c227d270a7586d62a3f34d144858216
Luke Olyslagers <luke.olyslagers@sitback.com.au>
2016-12-05 18:41:57 +1100

    Homepage markup and styling added

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,38 @@
-var gulp         = require("gulp"),
-    sass         = require("gulp-sass"),
+var gulp         = require('gulp'),
+    sass         = require('gulp-sass'),
+    notify       = require('gulp-notify'),
+    plumber      = require('gulp-plumber'),
     sourcemaps   = require('gulp-sourcemaps'),
     autoprefixer = require('gulp-autoprefixer'),
 
-    onError = function (err) {  
-  console.log(err);
+    onError = function (err) {
+      notify({
+        title: 'Gulp Task Error',
+        message: 'Check the console.'
+      }).write(err);
+
+      console.log(err.toString());
+       
+      this.emit('end');
  };
 
 
- var css_path  = 'dist/css/',
-     sass_path = 'scss/**/*.scss';
-
+  var css_path  = 'dist/css/',
+      sass_path = 'scss/**/*.scss',
+      bowerDir = './bower_components/';
 
 // Sass task
 gulp.task('sass', function() {
   gulp.src(sass_path)
+    .pipe(plumber({ errorHandle: onError }))
     .pipe(sourcemaps.init())
     .pipe(sass({
       outputStyle: 'nested',
+      includePaths: 'bower_components/bootstrap-sass/assets/stylesheets',
       precision: 10,
       errLogToConsole: true
     }))
+    .on('error', onError)
     .pipe(sourcemaps.write('dist/css/maps'))
     .pipe(gulp.dest(css_path));
 });
@@ -29,7 +41,7 @@ gulp.task('sass', function() {
 // Default task to be run with `gulp`
 gulp.task('default', ['sass'], function() {
   gulp.watch(sass_path, ['sass']);
-  gulp.watch("/*.html", ['browser-sync']);
+  gulp.watch('/*.html', ['browser-sync']);
 });
 
 
---
+++ js/form.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+  // Initialise Conditionize
+  $('.conditional').conditionize();
+
+  // Self desucting add button after click
+  $('.btn-self-destruct').on('click', function() {
+    $(this).remove();
+  });
+
+  // Auto populate additional children
+
+ $("#child_2_family_details").on('change', function() {
+  if ($(this).prop('checked')) {
+   
+    //get the values of the filled fields
+    $child1FamilyName = $('#child_1_family_name').val();
+    $child1Father = $('#child_1_father').val();
+    $child1Mother = $('#child_1_mother').val();
+    $child1SupportedBy = $('#child_1_supported_by').val();
+    $child1CurrentArrangementChanges = $('#child_1_current_arrangement_changes').val();
+    $child1CurrentArrangementChangesDetails = $('#child_1_current_arrangement_changes_details').val();
+
+    // Populate second child's details with data from first child
+    $.fn.copyChildDetails = function(child1Val) {
+      $(this).val(child1Val);
+      var $hasValue = this.val();
+      if ($hasValue) {
+        $(this).parent().addClass('has-success has-feedback');
+      } 
+
+    };
+
+    $('#child_2_family_name').copyChildDetails($child1FamilyName);
+    $('#child_2_father').copyChildDetails($child1Father);
+    $('#child_2_mother').copyChildDetails($child1Mother);
+    $('#child_2_supported_by').copyChildDetails($child1SupportedBy);
+    $('#child_2_current_arrangement_changes').copyChildDetails($child1CurrentArrangementChanges);
+    $('#child_2_current_arrangement_changes_details').copyChildDetails($child1CurrentArrangementChangesDetails);        
+
+    // $('#child_2_family_name').val($child1FamilyName);
+    // $('#child_2_father').val($child1Father);
+    // $('#child_2_mother').val($child1Mother);
+    // $('#child_2_supported_by').val($child1SupportedBy);
+    // $('#child_2_current_arrangement_changes').val($child1CurrentArrangementChanges);
+    // $('#child_2_current_arrangement_changes_details').val($child1CurrentArrangementChangesDetails);
+  } else {
+   $('#child_2_family_name').val('');
+   $('#child_2_father').val('');
+   $('#child_2_mother').val('');
+   $('#child_2_supported_by').val('');
+   $('#child_2_current_arrangement_changes').val('');
+   $('#child_2_current_arrangement_changes_details').val('');
+  }
+ });
+});
